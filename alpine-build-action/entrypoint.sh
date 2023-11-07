cebaa67c4f41d05fdbbfe3c54783ecbc6a8f44a0
requireCool <55184050+requireCool@users.noreply.github.com>
2023-11-07 16:47:31 +0800

    Update entrypoint.sh

--- alpine-build-action/entrypoint.sh
+++ alpine-build-action/entrypoint.sh
@@ -7,3 +7,4 @@ abuild-keygen -a -i -n
 # export PACKAGER_PUBKEY="${ABUILD_DIR}/$(ls -1rt ${ABUILD_DIR} | grep \.rsa\.pub | tail -n 1 | tail -n 1)"
 
 abuild -F checksum && abuild -F -r
+cp /github/*.apk ./

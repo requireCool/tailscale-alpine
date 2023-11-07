1dba45ce950b13b24102b0e9f07e99b93b5abc6f
requireCool <55184050+requireCool@users.noreply.github.com>
2023-11-07 23:59:39 +0800

    Update check_version.py

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -6,7 +6,7 @@ import requests
 tailscale_latest_release_url = 'https://api.github.com/repos/tailscale/tailscale/releases/latest'
 remote_version = requests.get(tailscale_latest_release_url).json()['name']
 
-with open('README.MD', 'r') as f:
+with open('README.md', 'r') as f:
   build_version = re.findall(r'Latest build version: ([0-9.]+)', f.read())[0]
 
 if remote_version == build_version:

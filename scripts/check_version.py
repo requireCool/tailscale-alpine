6a7141622a4bceda3592ad654a197cfe5b27766e
requireCool <55184050+requireCool@users.noreply.github.com>
2023-11-07 23:42:42 +0800

    Update check_version.py

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -5,7 +5,7 @@ import requests
 tailscale_latest_release_url = 'https://api.github.com/repos/tailscale/tailscale/releases/latest'
 version = requests.get(tailscale_latest_release_url).json()['name']
 
-if version == '1.52.1':
+if version == '1.52.2':
   print('false')
 else:
   print('true')

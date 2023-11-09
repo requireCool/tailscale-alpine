a5d46258c2ec1cea0ee8688c395336a9dadc9542
requireCool <55184050+requireCool@users.noreply.github.com>
2023-11-09 18:27:01 +0800

    Update check_version.py

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -9,12 +9,13 @@ def write_output(key: str, value: str):
 tailscale_latest_release_url = 'https://api.github.com/repos/tailscale/tailscale/releases/latest'
 remote_version = requests.get(tailscale_latest_release_url).json()['name']
 
+build_version = ''
 if os.path.exists('packages'):
   dirs = os.listdir('packages')
   dirs.sort(reverse=True)
   build_version = dirs[0]
-print(dirs)
-print(build_version)
+  print(dirs)
+  print(build_version)
 
 if remote_version == build_version:
   write_output('has_update', 'false')

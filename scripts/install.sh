c9849305703c33f90bd5644cb608af1d5cb6f11b
requireCool <55184050+requireCool@users.noreply.github.com>
2023-11-18 21:51:26 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,12 +3,12 @@ if command -v unzip > /dev/null 2>&1 ; then
   apk update && apk add unzip
 fi
 
-if command -v unzip > /dev/null 2>&1 ; then
+if command -v wget > /dev/null 2>&1 ; then
   apk update && apk add wget
 fi
 
 
-PKG_ZIP_URL=https://github.com/requireCool/tailscale-alpine/raw/master/packages/tailscale-1.54.0.zip
+PKG_ZIP_URL=https://cdn.jsdelivr.net/gh/requireCool/tailscale-alpine@master/packages/tailscale-1.54.0.zip
 
 wget $PKG_ZIP_URL -O tailscale.zip
 unzip tailscale.zip
@@ -16,3 +16,5 @@ unzip tailscale.zip
 service tailscale stop
 find . -name *.apk | xargs apk add --allow-untrusted
 service tailscale start
+
+rm -f tailscale.zip

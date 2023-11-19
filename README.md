a7c4295709aebfb7fc32b984f6c470dfc529d50f
requireCool <55184050+requireCool@users.noreply.github.com>
2023-11-19 03:02:29 +0000

    auto sync files with upstream

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ All build scripts came from alpine package repository.
 Packages are built by Github Actions, and available in 'packages' directory.
 
 Due to lack of signature, apks only can be installed with args --allow-untrusted
+
+Simply use:
+`curl https://cdn.jsdelivr.net/gh/requireCool/tailscale-alpine@master/scripts/install.sh | sh`
--- scripts/check_version.py
+++ scripts/check_version.py
@@ -1,26 +1,53 @@
 import os
 import requests
+from bs4 import BeautifulSoup
+
+
+has_update = False
 
 
 def write_output(key: str, value: str):
-  os.system(f'echo "{key}={value}" >> $GITHUB_OUTPUT')
+    os.system(f'echo "{key}={value}" >> $GITHUB_OUTPUT')
 
 
-tailscale_latest_release_url = 'https://api.github.com/repos/tailscale/tailscale/releases/latest'
-remote_version = requests.get(tailscale_latest_release_url).json()['name']
-print('Latest remote version:', remote_version)
+def check_update():
+    global has_update
 
-build_version = ''
-if os.path.exists('packages'):
-  dirs = os.listdir('packages')
-  dirs.sort(reverse=True)
-  if len(dirs) > 0:
-    build_version = dirs[0].split('-')[1].rstrip('.zip')
-    print('All dirs in packages:', dirs)
-    print('Latest build version:', build_version)
+    tailscale_latest_release_url = 'https://api.github.com/repos/tailscale/tailscale/releases/latest'
+    remote_version = requests.get(tailscale_latest_release_url).json()['name']
+    print('latest remote version:', remote_version)
 
-if remote_version == build_version:
-  write_output('has_update', 'false')
-else:
-  write_output('has_update', 'true')
-  write_output('pkgver', remote_version)
+    build_version = ''
+    if os.path.exists('packages'):
+        dirs = os.listdir('packages')
+        dirs.sort(reverse=True)
+        if len(dirs) > 0:
+            build_version = dirs[0].split('-')[1].rstrip('.zip')
+            print('all files in "packages":', dirs)
+            print('latest build version:', build_version)
+
+    if remote_version == build_version:
+        write_output('has_update', 'false')
+    else:
+        has_update = True
+        write_output('has_update', 'true')
+        write_output('pkgver', remote_version)
+
+
+def sync_files_with_upstream():
+    base_url = 'https://git.alpinelinux.org/aports/plain/community/tailscale'
+    bs = BeautifulSoup(requests.get(base_url).text, 'html.parser')
+    text_list = [li.text for li in bs.find_all('li')]
+    text_list.remove('../')
+
+    for name in text_list:
+        with open(name, 'wb') as f:
+            f.write(requests.get(base_url + '/' + name).content)
+        print(f'Sync {name} with upstream success.')
+
+
+if __name__ == '__main__':
+    check_update()
+    if not has_update:
+        exit()
+    sync_files_with_upstream()

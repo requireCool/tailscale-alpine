4593ab4dc95ddfe260b7591fae191e2d3cb13bc8
requireCool <55184050+requireCool@users.noreply.github.com>
2023-12-30 08:09:01 +0000

    load tun

--- alpine-build-action/entrypoint.sh
+++ alpine-build-action/entrypoint.sh
@@ -1,7 +1,14 @@
+# load tun
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+ls /dev/net && cat /dev/net/tun
+
+# run abuild
 cd src
-echo $PWD
 abuild-keygen -a -i -n
 abuild -F checksum && abuild -F -r
 
+# copy apk packages
 mkdir packages
 find / -name tailscale-*.apk -exec cp {} packages \;

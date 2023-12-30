43e356deef7dee97c5acf1523c08c2922b51f039
requireCool <55184050+requireCool@users.noreply.github.com>
2023-12-30 08:44:52 +0000

    fix build file

--- alpine-build-action/entrypoint.sh
+++ alpine-build-action/entrypoint.sh
@@ -1,11 +1,24 @@
+cd src
+
 # load tun
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 chmod 666 /dev/net/tun
-ls /dev/net && cat /dev/net/tun
+
+# replace version if needed
+if [[ $PKGVER != $(cat APKBUILD | grep "pkgver=") ]]; then
+    echo "set pkgver to $PKGVER"
+    sed -i "s/pkgver=.*/pkgver=$PKGVER/g" APKBUILD
+fi
+if [[ $PKGREL != $(cat APKBUILD | grep "pkgrel=") ]]; then
+    echo "set pkgrel to $PKGREL"
+    sed -i "s/pkgrel=.*/pkgrel=$PKGREL/g" APKBUILD
+fi
+
+# skip go test
+sed -i 's/go test/# &/' APKBUILD
 
 # run abuild
-cd src
 abuild-keygen -a -i -n
 abuild -F checksum && abuild -F -r
 

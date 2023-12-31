e7048ef3841e32f438ebe6f7aa0a78d7083c4d66
requireCool <55184050+requireCool@users.noreply.github.com>
2023-12-31 02:24:46 +0000

    debug

--- alpine-build-action/entrypoint.sh
+++ alpine-build-action/entrypoint.sh
@@ -8,19 +8,19 @@ cd src
 # replace version if needed
 if [[ "pkgver=$PKGVER" != $(cat APKBUILD | grep "pkgver=") ]]; then
     echo "set pkgver to $PKGVER"
-    sed -i "s/pkgver=.*/pkgver=$PKGVER/g" APKBUILD
+    doas sed -i "s/pkgver=.*/pkgver=$PKGVER/g" APKBUILD
 fi
 if [[ "pkgrel=$PKGREL" != $(cat APKBUILD | grep "pkgrel=") ]]; then
     echo "set pkgrel to $PKGREL"
-    sed -i "s/pkgrel=.*/pkgrel=$PKGREL/g" APKBUILD
+    doas sed -i "s/pkgrel=.*/pkgrel=$PKGREL/g" APKBUILD
 fi
 
 # skip go test
 # sed -i 's/go test/msg2 "Skip check..."\n# &/' APKBUILD
 
 # run abuild
-abuild-keygen -a -i -n
-abuild -F checksum && abuild -F -r
+doas abuild-keygen -a -i -n
+doas abuild -F checksum && doas abuild -F -r
 
 # copy apk packages
 cd ..
